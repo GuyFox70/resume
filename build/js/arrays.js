8a634fe9408023a5f0e89c44ba67d5c5637b4733
Artem Orlov <artm.orlov.88@list.ru>
2020-01-02 01:55:45 +0300

    add plugins for gulp and rewritten gulpfile.js

--- build/js/arrays.js
+++ build/js/arrays.js
@@ -1,10 +1,1 @@
-let arr_description = [
-    ['akjdkal', 'ksdfsj', 'dskflkj'],
-    ['jhfs', 'sa', 'sd']
-];
-
-let arr_name_project = ['Five in row (game)', 'lenoma.ru'];
-
-let arr_link_img = ['build/img/five.png', 'build/img/lenoma.png'];
-
-let arr_paths = ['five_in_row/index.html', 'lenoma_project/index.html'];
+let arr_description=[["akjdkal","ksdfsj","dskflkj"],["jhfs","sa","sd"]],arr_name_project=["Five in row (game)","lenoma.ru"],arr_link_img=["img/five.png","img/lenoma.png"],arr_paths=["five_in_row/index.html","lenoma_project/index.html"];
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require('gulp');
+
 //css
 const concatCss = require('gulp-concat-css');
 const autoprefixer = require('gulp-autoprefixer');
@@ -10,16 +11,19 @@ const imageminJpg = require('imagemin-jpeg-recompress');
 const imageminPng = require('imagemin-optipng');
 
 //js
-// const uglify = require('gulp-uglify');
+const terser = require('gulp-terser');
 
 // clean
 const clean = require('gulp-clean');
 
+// browser-sync
+var browserSync = require('browser-sync').create();
+
 // browser-sync start --server --files "*.*"
 // browser-sync start --proxy "myproject" --files "*.css, *.html, *.php, *.js"
-// npm uninstall <pa
 
 const cssFile = [
+    './src/css/fonts.css',
     './src/css/core.css',
     './src/css/info.css',
     './src/css/education.css',
@@ -38,6 +42,32 @@ const jsFile = [
     './src/js/swipe.js'
 ];
 
+gulp.task('html', function () {
+    return gulp.src('./src/index.html')
+
+    .pipe(gulp.dest('./build'))
+})
+
+gulp.task('fonts', function () {
+    return gulp.src('./src/css/fonts/*.*')
+
+    .pipe(gulp.dest('./build/css/fonts'))
+})
+
+gulp.task('styles', function () {
+    return gulp.src(cssFile)
+
+    .pipe(concatCss('style.css'))
+    .pipe(autoprefixer({
+        cascade: false
+    }))
+    .pipe(cleanCSS({
+        level: 2
+    }))
+
+    .pipe(gulp.dest('./build/css'))
+})
+
 gulp.task('images', function () {
     return gulp.src('src/img/*')
 
@@ -55,29 +85,41 @@ gulp.task('images', function () {
     .pipe(gulp.dest('build/img'))
 })
 
-gulp.task('styles', function () {
-    return gulp.src(cssFile)
-
-    .pipe(concatCss('style.css'))
-    .pipe(autoprefixer({
-        cascade: false
-    }))
-    .pipe(cleanCSS({
-        level: 2
-    }))
-
-    .pipe(gulp.dest('./build/css'))
-})
-
 gulp.task('scripts', function () {
     return gulp.src(jsFile)
 
-    .pipe(gulp.dest('./build/js'))
+    .pipe(terser({
+        keep_fnames: true,
+        mangle: false,
+        compress: true
+    }))
+
+    .pipe(gulp.dest('./build/js'));
 })
 
-gulp.task('clean', function () {
-    return gulp.src('build/img/*.db', {read: false})
-        .pipe(clean());
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./build"
+        }
+    });
+    browserSync.watch('build', browserSync.reload)
 });
 
-// gulp.task('build', gulp.series(clean,gulp.parallel(styles,scripts,images)));
+
+gulp.task('clean', function () {
+    return gulp.src('build/js/*', {read: false})
+        .pipe(clean())
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/index.html', gulp.series('html'));
+    gulp.watch('src/css/fonts/font.css', gulp.series('fonts'));
+    gulp.watch('src/css/*.css', gulp.series('styles'));
+    gulp.watch('src/js/*.js', gulp.series('scripts'));
+});
+
+gulp.task('default', gulp.series(
+    gulp.parallel('html','fonts','styles','scripts'),
+    gulp.parallel('watch','serve')
+));
